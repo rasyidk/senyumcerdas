629e6e78b209b2b4fe50fb55536bae3247413869
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2024-07-12 13:18:37 +0700

    first commit

--- src/app/components/partials/Navbar.js
+++ src/app/components/partials/Navbar.js
@@ -6,12 +6,7 @@ export default function Navbar() {
     <div className=" bg-white absolute z-20 w-full">
       <div className="container mx-auto flex justify-between items-center h-full">
         <div className="w-full flex justify-center lg:justify-start my-4">
-          <Image
-            src="/assets/logo.svg" // Path to your logo image in the public directory
-            alt="Logo"
-            width={80} // Adjust the width of the image
-            height={50} // Adjust the height of the image
-          />
+          <p className="text-primary font-bold text-3xl">Senyum Cerdas</p>
         </div>
       </div>
     </div>
